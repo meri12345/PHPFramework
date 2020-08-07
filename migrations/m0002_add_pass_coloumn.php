ccb074118baff8dfa5570b59ac59b858cddd09b3
meri12345 <38717612+meri12345@users.noreply.github.com>
2020-08-07 11:07:50 +0200

    registration

--- migrations/m0002_add_pass_coloumn.php
+++ migrations/m0002_add_pass_coloumn.php
@@ -3,7 +3,7 @@
 class m0002_add_pass_coloumn{
     public function up(){
         $db = \app\core\Application::$app->db;
-        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR (24) NOT NULL");
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR (1028) NOT NULL");
     }
 
     public function down(){
--- models/User.php
+++ models/User.php
@@ -4,18 +4,25 @@
 namespace app\models;
 
 
-use app\core\Model;
+use app\core\DbModel;
 
-class RegisterModel extends Model
+class User extends DbModel
 {
+    public const STATUS_INACTIVE=0;
+    public const STATUS_ACTIVE=1;
+    public const STATUS_DELETED=2;
+
     public string $firstname = '';
     public string $lastname = '';
     public string $email = '';
+    public $status = self::STATUS_INACTIVE;
     public string $password = '';
     public string $passwordConfirm = '';
 
-    public function register(){
-        echo "Creating user";
+    public function save(){
+        $this->password=password_hash($this->password,PASSWORD_DEFAULT);
+
+       return parent::save();
     }
 
     public function rules(): array
@@ -23,9 +30,19 @@ class RegisterModel extends Model
         return [
           'firstname'=>[self::RULE_REQUIRED],
             'lastname'=>[self::RULE_REQUIRED],
-            'email'=>[self::RULE_EMAIL,self::RULE_REQUIRED],
+            'email'=>[self::RULE_REQUIRED],
             'password'=>[self::RULE_REQUIRED,[self::RULE_MIN,'min'=>'8'],[self::RULE_MAX,'max'=>'16']],
             'passwordConfirm'=>[self::RULE_REQUIRED,[self::RULE_MATCH,'match'=>'password']]
         ];
+    }
+
+    public function tableName(): string
+    {
+        return 'users';
+    }
+
+    public function attributes(): array
+    {
+       return ['firstname','lastname','email','password','status'];
     }
 }
